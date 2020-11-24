ce413f44ad9cf9d9fe45a5384e6fbcbe8613e9d6
Nelson <nelsonx73@gmail.com>
2020-11-23 22:33:31 -0500

    Add existing file

---
+++ Repositories/MongoDbItemsRepository.cs
@@ -0,0 +1,46 @@
+﻿using MongoDB.Driver;
+using net5_catalog.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace net5_catalog.Repositories
+{
+    public class MongoDbItemsRepository : IItemsRepository
+    {
+
+        private readonly IMongoCollection<Item> itemsCollection;
+
+        public MongoDbItemsRepository(IMongoClient mongoClient)
+        {
+            IMongoDatabase database = mongoClient.GetDatabase("catalog");
+            itemsCollection = database.GetCollection<Item>("items");
+        }
+
+        public void CreateItem(Item item)
+        {
+            itemsCollection.InsertOne(item);
+        }
+
+        public void DeleteItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Item GetItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Item> GetItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateItem(Item item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
